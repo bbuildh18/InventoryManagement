05308abbf8f5f3ba79b1aecd5d40e997d57574ab
prageeth <prageeth.madhu@gmail.com>
2018-10-12 09:28:15 +0530

    changes

--- InventoryAPI/Startup.cs
+++ InventoryAPI/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-
+//
 namespace InventoryAPI
 {
     public class Startup
